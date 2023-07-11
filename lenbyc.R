b99c53e3d0ebac9555b4a723686d761d57d4f8f4
mathiashole <joacomangino@gmail.com>
2023-07-11 11:26:13 -0300

    add args and other value in plot

--- lenbyc.R
+++ lenbyc.R
@@ -1,41 +1,65 @@
+#!/usr/bin/env Rscript
+
+
+# Verify that the necessary arguments have been specified.
+if (length(args) != 1) {
+  stop("Use program: length_of_contig.txt")
+}
+
+
+#library(tidyverse)
+#library(tidytext)
+#library(dplyr)
+
 ##library to use________________________________________________________________
-library(tidyverse)
-library(ggplot2)
+packages <- c("ggplot2")
 
+# Check if the packages are installed
 
-##We set directory______________________________________________________________
-setwd("/home/usuario/acca/data_base/")
+for (package in packages) {
+  if (requireNamespace(package, quietly = TRUE)) {
+    # El paquete está instalado, cargarlo con library()
+    library(package, character.only = TRUE)
+  } else {
+    # The package is not installed, install it and load it with library().
+    install.packages(package)
+    library(package, character.only = TRUE)
+  }
+}
+
+#library(tidyverse)
+#library(ggplot2)
+
+#setwd("/home/usuario/acca/data_base/")
 
 ##Read our data_________________________________________________________________
 
-contig <- read.table("lentgh_of_contig_acca.txt",
+contig <- read.table(args[1], #"lentgh_of_contig_acca.txt"
                    sep = "\t",
                    stringsAsFactors = T,
                    header = T)
 
+# NEED CONDITIONS
 
 ##Graphics section______________________________________________________________
 
 ##Histogram_____________________________________________________________________
 
-##Histogram length RT candiadtes athila/tat_____________________________________
+##Histogram length______________________________________________________________
 
 n_contig <- length(contig$Contig.id)
-n_write <- paste("nº total contig = ", n_contig)
+n_write <- paste("Total nº of contigs = ", n_contig)
+n_binwidth <- round(mean(contig$Contig.length))/10^6
 
-candidate_RT_length <- ggplot(contig, aes(x= Contig.length, fill= Contig.length)) + 
-  geom_histogram(binwidth = 100000, fill="gray21", color = "gray1", alpha=0.9, position = "identity")+
+length <- ggplot(contig, aes(x= Contig.length/10^6, fill= Contig.length)) + 
+  geom_histogram(binwidth = n_binwidth, fill="gray21", color = "gray1", alpha=0.9, position = "identity")+
   #geom_density(aes(y = ..count..*1500),adjust = 2, col = "black", fill = "gray1", alpha= 0.2)+
   theme(legend.position="none",
         plot.title = element_text(size=11), 
         panel.background = element_rect(fill = "white",
                                         colour ="grey50")) +
-  labs(y = n_write , x = "Largo total de contig (pb)") 
+  labs(y = n_write , x = "Total length of contig (mb)") 
 
-length(candidate_total_rt$Length)
-mean(candidate_total_rt$Length)##There are many trash sequence!!!!!!!
-sd(candidate_total_rt$Length)
 
-ks.test(candidate_total_rt$Length, pnorm, mean(candidate_total_rt$Length), sd(candidate_total_rt$Length))
-
+ks.test(contig$Contig.length, pnorm, mean(contig$Contig.length), sd(contig$Contig.length))
 
