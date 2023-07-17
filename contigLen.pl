11bfa9cc97d14c33ddcb7bb48f09be3fb249e058
mathiashole <joacomangino@gmail.com>
2023-07-17 19:07:33 -0300

    add R script in perl program

--- contigLen.pl
+++ contigLen.pl
@@ -2,6 +2,8 @@
 
 use strict;
 
+use FindBin qw($Bin);
+
 sub calculate_contig_lengths {
     my $fasta_file = shift;
     open(my $fh, "<", $fasta_file) or die "Cannot open $fasta_file: $!";
@@ -31,8 +33,32 @@ sub calculate_contig_lengths {
 my $fasta_file = shift;
 my @contig_lengths = calculate_contig_lengths($fasta_file);
 
-print "Contig id\tContig length\n";
+#print "Contig id\tContig length\n";
+#foreach my $contig (@contig_lengths) {
+#    my ($name, $length) = @$contig;
+#    print "$name\t$length\n";
+#}
+
+# Initialize a variable to store the output
+my $output = "Contig id\tContig length\n";
+
+# Construir la salida en formato de tabla
 foreach my $contig (@contig_lengths) {
     my ($name, $length) = @$contig;
-    print "$name\t$length\n";
+    $output .= "$name\t$length\n";
 }
+
+# Save the output to a temporary file
+my $output_file = 'output.txt';
+open(my $fh, '>', $output_file) or die "No se pudo abrir el archivo: $!";
+print $fh $output;
+close($fh);
+
+# Construct the path to the R script file
+my $script_r = "$Bin/lenbyc.R";
+
+# Command in R to be executed
+my $comando_r = "Rscript $script_r \"$output_file\"";
+
+# Run the R command
+system($comando_r);
