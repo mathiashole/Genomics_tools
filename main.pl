b0c5c0529fef932b36627fa5d2fae12fa35917dd
mathiashole <joacomangino@gmail.com>
2023-07-21 10:29:58 -0300

    add contig length program in main

--- main.pl
+++ main.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 
 use strict;
+use FindBin qw($Bin);
 
 # Function to show help
 sub show_help {
@@ -10,6 +11,7 @@ Use: GStat [OPTIONS]
  Opciones disponibles:
     -h, --help     Show this help.
     -v, --version  Show the version of the program.
+    -l, --length   Show and save lengths of contigs
     # Missing options
 
  Examples:
@@ -30,6 +32,17 @@ if (scalar(@ARGV) == 0) {
     show_help();
 } elsif ($ARGV[0] eq '-v' || $ARGV[0] eq '--version') {
     show_version();
+} elsif ($ARGV[0] eq '-l' || $ARGV[0] eq '--length') {
+    
+    # Construct the path to the perl script file
+    my $script_length = "$Bin/contigLen.pl";
+
+    # Command in perl to be executed
+    my $comando_length = "perl $script_length \"$fasta_file\\";
+
+    # Run the perl command
+    system($comando_length);
+
 } else {
     print "Unknown option. Use '-h' or '--help' to display help.\n";
 }
