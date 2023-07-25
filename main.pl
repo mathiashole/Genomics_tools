46cc99aef2edd921958064b5cd1152ff46db94ff
mathiashole <joacomangino@gmail.com>
2023-07-24 21:13:45 -0300

    add N50 program in main.pl

--- main.pl
+++ main.pl
@@ -62,6 +62,38 @@ if (scalar(@ARGV) == 0) {
     # Run the perl command
     system($comando_length);
 
+} elsif ($ARGV[0] eq '-n50') {
+
+    # Verify that a second argument is supplied
+    die "Error: Missing FASTA file. Usage: perl main.pl -n50 <fasta_file>\n" unless @ARGV == 2;
+
+    # Get the name of the FASTA file given as an argument
+    my $fasta_file = $ARGV[1];
+
+    # Verify that the file exists
+    die "Error: File '$fasta_file' not found.\n" unless -e $fasta_file; # -e chack for file existence
+
+    # $fasta_file path of file
+    # Verify that the file has a .fasta or .fa extension
+    my ($file_name, $file_path, $file_ext) = fileparse($fasta_file, qr/\.[^.]*/); # \. = dot in file name. [^.]* = any sequence followed by a dot
+    
+    # fileparse() parse the text and save it in a list of variables
+    #    $file_name = sample
+    #    $file_path = /ruta/del/archivo/fasta/
+    #    $file_ext = .fasta
+    die "Error: File '$fasta_file' is not in FASTA format.\n" unless $file_ext =~ /^\.fasta|\.fa$/i;
+
+    # Construct the path to the perl script file
+    my $script_n50 = "$Bin/GStat.pl";
+
+    # Command in perl to be executed
+    my $comando_n50 = "perl $script_n50 \"$fasta_file\\";
+
+    # Run the perl command
+    system($comando_n50);
+
 } else {
+
     print "Unknown option. Use '-h' or '--help' to display help.\n";
+    
 }
