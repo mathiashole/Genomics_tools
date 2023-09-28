fd90f564899d96102b9cf36c475df882bc567fb4
mathiashole <joacomangino@gmail.com>
2023-09-27 22:28:50 -0300

    add new function, calculate %AT

--- contigStat.pl
+++ contigStat.pl
@@ -6,7 +6,7 @@ use FindBin qw($Bin);
 my @contig_data;
 my $contig;
 
-sub calculate_contig_lengths_gc {
+sub calculate_contig_lengths_gc_at {
     my $fasta_file = shift;
     open(my $fh, "<", $fasta_file) or die "Error\tCannot open $fasta_file: $!\n";
     my @contig_data;
@@ -18,7 +18,8 @@ sub calculate_contig_lengths_gc {
         if ($line =~ /^>/) {
             if ($current_name ne "") {
                 my $gc_percentage = calculate_gc_percentage($current_sequence);
-                push @contig_data, [$current_name, $sequence_length, $gc_percentage];
+                my $at_percentage = calculate_at_percentage($current_sequence);
+                push @contig_data, [$current_name, $sequence_length, $gc_percentage, $at_percentage];
                 $current_sequence = "";
                 $sequence_length = 0;
             }
@@ -31,7 +32,8 @@ sub calculate_contig_lengths_gc {
     }
     if ($current_name ne "") {
         my $gc_percentage = calculate_gc_percentage($current_sequence);
-        push @contig_data, [$current_name, $sequence_length, $gc_percentage];
+        my $at_percentage = calculate_at_percentage($current_sequence);
+        push @contig_data, [$current_name, $sequence_length, $gc_percentage, $at_percentage];
     }
     close $fh;
     return @contig_data;
@@ -46,17 +48,25 @@ sub calculate_gc_percentage {
     return $gc_percentage;
 }
 
+sub calculate_at_percentage {
+    my $sequence = shift;
+    my $at_count = ($sequence =~ tr/ATat/ATat/);
+    my $sequence_length = length($sequence);
+    my $at_percentage = ($at_count / $sequence_length) * 100;
+    return $at_percentage;
+}
+
 my $fasta_file = shift;
-my @contig_lengths = calculate_contig_lengths_gc("$fasta_file");
+my @contig_lengths = calculate_contig_lengths_gc_at("$fasta_file");
 
 # Initialize a variable to store the output
-my $output = "id\tlength\tGC\n";
+my $output = "id\tlength\tGC\tAT\n";
 
 # Construir la salida en formato de tabla
 foreach my $contig (@contig_lengths) {
-    my ($name, $length, $gc) = @$contig;
-    $output .= "$name\t$length\t$gc\n";
-    print "$name\t$length\t$gc\n";
+    my ($name, $length, $gc, $at) = @$contig;
+    $output .= "$name\t$length\t$gc\t$at\n";
+    print "$name\t$length\t$gc\t$at\n";
 }
 
 # Save the output to a temporary file
